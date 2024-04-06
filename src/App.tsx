c8f872b217361e93d3dd1f291599ee0d58298e71
Yasser Akchayat <akchayaty333@gmail.com>
2024-04-06 04:42:02 +0200

    fixing types problem2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 import ShoppingCartSidebar from './components/ShoppingCartSidebar';
